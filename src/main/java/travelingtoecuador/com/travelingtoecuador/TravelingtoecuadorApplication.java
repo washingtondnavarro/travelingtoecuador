58fd5b803d25b4116060cd0efc7b7d216197c18b
washingtondnavarro <washington.d.navarro@gmail.com>
2018-03-01 13:19:25 -0600

    first commit and adding landing page

---
+++ src/main/java/travelingtoecuador/com/travelingtoecuador/TravelingtoecuadorApplication.java
@@ -0,0 +1,12 @@
+package travelingtoecuador.com.travelingtoecuador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelingtoecuadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelingtoecuadorApplication.class, args);
+	}
+}
